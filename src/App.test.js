ee799c9acfb51433b0c9641a73ec6a4ba970ba70
oussemabes <oussema.besbes@supcom.tn>
2023-02-05 21:45:32 +0100

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import About from '../src/components/About';
+const { Builder, By, Key, until,WebDriver } = require("selenium-webdriver");
 
-test('renders learn react link', () => {
-  render(<About />);
-  const linkElement = screen.getByText(/about/i);
-  expect(linkElement).toBeInTheDocument(); 
-});
+describe("React App E2E Tests", () => {
+  let driver;
+
+  beforeEach(async () => {
+    driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("http://localhost:3000/login");
+  });  
+  afterEach(async () => {
+    await driver.quit();
+  });
+   it("should login succesfly and navigate to home", async () => {
+    let initialUrl=await driver.getCurrentUrl() 
+    let currentUrl 
+    const inputField = await driver.findElement(By.name("email"));
+    await inputField.sendKeys("oussema422oussema@gmail.com");
+    const button = await driver.findElement(By.name("button"));
+    const passwordField = await driver.findElement(By.name("password"));
+    await passwordField.sendKeys("123456789a");
+    await button.click().then(currentUrl =await driver.getCurrentUrl());
+
+    await driver.wait(async () => {
+      const currentUrl = await driver.getCurrentUrl();
+      return currentUrl !== initialUrl; 
+  }, 1000);
+  const updatedUrl = await driver.getCurrentUrl();
+  
+    expect(updatedUrl).toBe("http://localhost:3000/")
+    
+  }); 
+
+})
  
